bfb0b692bf16f33ca782245381bf58cd42cf1d65
srmcconomy <srmcconomy@gmail.com>
2015-01-27 00:37:56 -0500

    added icons

--- bingohelper.js
+++ bingohelper.js
@@ -1,3 +1,17 @@
+var rowElements = {};
+rowElements["row1"] = [1,2,3,4,5];
+rowElements["row2"] = [6,7,8,9,10];
+rowElements["row3"] = [11,12,13,14,15];
+rowElements["row4"] = [16,17,18,19,20];
+rowElements["row5"] = [21,22,23,24,25];
+rowElements["col1"] = [1,6,11,16,21];
+rowElements["col2"] = [2,7,12,17,22];
+rowElements["col3"] = [3,8,13,18,23];
+rowElements["col4"] = [4,9,14,19,24];
+rowElements["col5"] = [5,10,15,20,25];
+rowElements["tlbr"] = [1,7,13,19,25];
+rowElements["bltr"] = [5,9,13,17,21];
+
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -28,6 +42,13 @@ var bingoOpts = {
 var bingoBoard = ootBingoGenerator(bingoList, bingoOpts);
 
 function dif() {
+	for (row in rowElements) {
+		var sum = 0;
+		for (i of rowElements[row]) {
+			sum += bingoBoard[i].difficulty;
+		}
+		$('#' + row).append(sum);
+	}
 	for (var i=1; i<=25; i++) {
 		document.getElementById("slot"+i).style.background = "rgb(0,"+ bingoBoard[i].difficulty*10 + ",0)";
 	}
